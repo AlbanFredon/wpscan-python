e74ccd3507d1c3415f8fcbb0a1d7c3d2ec5e0700
AlbanFredon <121691987+AlbanFredon@users.noreply.github.com>
2023-01-01 20:33:27 +0100

    Update wpscan_information.py

--- wpscan_information.py
+++ wpscan_information.py
@@ -1,42 +1,50 @@
 import requests
 import re
 
-def get_wp_version(url):
+def get_theme_info(url):
   # Envoie une requête GET à l'URL cible et récupère la page HTML
   html = requests.get(url).text
 
-  # Utilise une expression régulière pour extraire la version de WordPress de la page HTML
-  match = re.search(r"<meta name='generator' content='WordPress (.*?)' />", html)
-  if match:
-    return match.group(1)
-  return None
+  # Utilise une expression régulière pour extraire le nom du thème de la page HTML
+  name_match = re.search(r"<h1 class='theme-name'>(.*?)</h1>", html)
+  if name_match:
+    name = name_match.group(1)
+  else:
+    name = None
 
-def get_plugin_list(url):
-  # Envoie une requête GET à l'URL cible avec le paramètre "api"
-  # Cela permet de récupérer la liste des plugins sous forme de données JSON
-  api_url = url + "/wp-json/wp/v2/plugins"
-  data = requests.get(api_url).json()
+  # Utilise une expression régulière pour extraire la version du thème de la page HTML
+  version_match = re.search(r"<span class='theme-version'>(.*?)</span>", html)
+  if version_match:
+    version = version_match.group(1)
+  else:
+    version = None
 
-  # Crée une liste des noms de plugins à partir des données JSON
-  plugin_list = []
-  for plugin in data:
-    plugin_list.append(plugin['name'])
-  return plugin_list
+  # Envoie une requête GET au fichier readme.txt du thème
+  readme_url = url + "/readme.txt"
+  readme = requests.get(readme_url).text
+
+  # Utilise une expression régulière pour extraire la description du thème du fichier readme.txt
+  description_match = re.search(r"== Description ==(.*?)==", readme, re.DOTALL)
+  if description_match:
+    description = description_match.group(1).strip()
+  else:
+    description = None
+
+  return {
+    "name": name,
+    "version": version,
+    "description": description
+  }
 
 def main():
   # Demander à l'utilisateur de saisir l'URL cible
   target_url = input("Enter the target URL: ")
 
-  # Afficher la version de WordPress cible
-  version = get_wp_version(target_url)
-  if version:
-    print("WordPress version:", version)
-  else:
-    print("Unable to determine WordPress version.")
-
-  # Afficher la liste des plugins installés sur le site cible
-  plugin_list = get_plugin_list(target_url)
-  print("Installed plugins:", ", ".join(plugin_list))
+  # Récupérer les informations du thème à partir de l'URL cible
+  theme_info = get_theme_info(target_url)
+  print("Name:", theme_info['name'])
+  print("Version:", theme_info['version'])
+  print("Description:", theme_info['description'])
 
 if __name__ == "__main__":
   main()
